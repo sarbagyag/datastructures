fa57c23d410f3d3fab62962bfd8dd9fad07c2d6d
sarbagya <sarbagyaghoshrestha@gmail.com>
2026-05-11 20:53:00 +0545

    feat: update gitignore to ignore the object files

--- ds.h
+++ ds.h
@@ -13,4 +13,14 @@ void *list_pop(List *list);
 void list_free(List *list);
 int list_size(List *list);
 
+// Vector
+
+typedef struct Vector Vector;
+
+Vector *vec_create(void);
+void vec_push(Vector *v, int index);
+void *vec_get(Vector *v, int index);
+int vec_size(Vector *v);
+void vec_free(Vector *v);
+
 #endif
